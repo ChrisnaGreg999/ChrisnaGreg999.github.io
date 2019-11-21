f10680cd38171beed39e72dafac4212f70bb7451
ChrisnaGre999 <chrisnamahendra@gmail.com>
2019-11-21 12:02:31 +0700

    initial

---
+++ precache-manifest.b564ca38acd2f3cb37d9a5d94a21bc6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615dfe8e4760054b3af7",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "d8f5b1e30c988c5b31f7",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "5564826e2e4480c6f6397a7cdf0e8138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615dfe8e4760054b3af7",
+    "url": "/js/about.4fdeb010.js"
+  },
+  {
+    "revision": "59df129add1a8f7eaf97",
+    "url": "/js/app.9f0f4ce4.js"
+  },
+  {
+    "revision": "d8f5b1e30c988c5b31f7",
+    "url": "/js/chunk-vendors.833eec04.js"
+  },
+  {
+    "revision": "403a711b1d57b8a96e84fde108e002db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
